f84d48091d2ac3faaaedc9b0ca256550eef144fc
Tiago Cunha <1150807@isep.ipp.pt>
2017-05-31 11:13:42 +0100

    constructor EventEmployee [Implementation]

--- src/main/java/lapr/project/model/EventEmployee.java
+++ src/main/java/lapr/project/model/EventEmployee.java
@@ -4,6 +4,13 @@ package lapr.project.model;
  *
  * @author 
  */
-public class EventEmployee {
+public class EventEmployee extends User{
+
+    public EventEmployee(String m_sName, String m_sEmail, String m_sUserName, String m_sPassword) {
+        super(m_sName, m_sEmail, m_sUserName, m_sPassword);
+    }
+
+    public EventEmployee() {
+    }
     
 }
