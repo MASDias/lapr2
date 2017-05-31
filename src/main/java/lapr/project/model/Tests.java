24150bb2693d0f7f682c155d736ea2ed10d6223b
Mario Dias [1151708] <MarioDias@LAPTOP-UM1MEFTM>
2017-05-31 12:01:27 +0100

    Test Event Manager [Implementation]

--- src/main/java/lapr/project/model/Tests.java
+++ src/main/java/lapr/project/model/Tests.java
@@ -18,7 +18,11 @@ public class Tests {
         User u1 = new User("Abilio Madureira", "abiliom", "abiliomadureira@gmail.com", "123abc123");
         User u2 = new User("Gonçalo Fonseca", "goncalof", "goncalofonseca@hotmail.com", "abc123abc");
         User u3 = new User("Emilio Carvalhosa", "emilioc", "emiliocarvalhosa@sapo.pt", "qwerty");
-        
+        if (EventManagerCreatorTest()) {
+            System.out.println("Success Creating Event Manager");
+        }else{
+            System.out.println("Failed to create Event Manager");
+        }
         System.out.println(u1);
         System.out.println("");
         System.out.println(u2);
@@ -26,5 +30,14 @@ public class Tests {
         System.out.println(u3);
 
     }
-    
+
+    public static boolean EventManagerCreatorTest() {
+        EventManager eManager = new EventManager("Mario Dias", "md@gmail.com", "mdias", "123");
+        
+        if (eManager != null) {
+            return true;
+        }
+        return false;
+    }
+
 }
