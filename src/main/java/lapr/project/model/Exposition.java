804fc6eda918c1f764d16efad1d3c532ae62ac44
MarioDias <MarioDias@LAPTOP-UM1MEFTM>
2017-05-31 11:30:37 +0100

    Update Event Manager [Implementation]

--- src/main/java/lapr/project/model/Exposition.java
+++ src/main/java/lapr/project/model/Exposition.java
@@ -1,9 +1,18 @@
 package lapr.project.model;
 
+import java.util.Date;
+
 /**
  *
  * @author 
  */
-public class Exposition {
+public class Exposition extends Event{
+
+    public Exposition() {
+    }
+
+    public Exposition(String m_sTitle, String m_sDescription, Date m_dEventBegin, Date m_dEventEnd, Local m_lLocal) {
+        super(m_sTitle, m_sDescription, m_dEventBegin, m_dEventEnd, m_lLocal);
+    }
     
 }
