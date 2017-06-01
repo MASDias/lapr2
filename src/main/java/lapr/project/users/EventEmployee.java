2b8f177431d833852943745d094d6f17b97f40f3
Mario Dias [1151708] <1151708@isep.ipp.pt>
2017-06-01 11:15:24 +0100

    Update package Users name [Implementation]

---
+++ src/main/java/lapr/project/users/EventEmployee.java
@@ -0,0 +1,17 @@
+package lapr.project.users;
+
+/**
+ *
+ * @author 
+ */
+public class EventEmployee extends User{
+
+    public EventEmployee(String m_sName, String m_sEmail, String m_sUserName, String m_sPassword) {
+        super(m_sName, m_sEmail, m_sUserName, m_sPassword);
+    }
+
+    public EventEmployee() {
+        
+    }
+    
+}
---
+++ src/main/java/lapr/project/users/Organizer.java
@@ -0,0 +1,16 @@
+package lapr.project.users;
+
+/**
+ *
+ * @author MarioDias
+ */
+public class Organizer extends User{
+    
+    public Organizer(String name, String email, String username, String password){
+        super(name, email, username, password);
+    }
+    
+    public Organizer(){
+        
+    }
+}
