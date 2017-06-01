61ea9fe80629f4a1c63288601b4e97de7dd21ec3
Mario Dias [1151708] <1151708@isep.ipp.pt>
2017-06-01 14:56:46 +0100

    Correction to tests EventEmployee

--- src/test/java/lapr/project/users/EventEmployeeTest.java
+++ src/test/java/lapr/project/users/EventEmployeeTest.java
@@ -17,20 +17,20 @@ public class EventEmployeeTest {
 
     @Test
     public static void EnsureSameObjectEventEmployeeIsEqual() {
-        EventEmployee eEmployee = new EventEmployee("Miguel", "miguel@gmail.com", "miguel4", "miguel123");
+        EventEmployee eEmployee = new EventEmployee("Miguel", "miguel@gmail.com", "miguel4", "miguel123",3);
         assertEquals(eEmployee, eEmployee);
     }
 
     @Test
     public static void EnsureSameObjectsEventEmployeeAreNotEqual() {
-        EventEmployee eEmployee = new EventEmployee("Miguel", "miguel@gmail.com", "miguel4", "miguel123");
-        EventEmployee expectedResult = new EventEmployee("Z", "c", "b", "a");
+        EventEmployee eEmployee = new EventEmployee("Miguel", "miguel@gmail.com", "miguel4", "miguel123",3);
+        EventEmployee expectedResult = new EventEmployee("Z", "c", "b", "a",3);
         assertNotEquals(eEmployee, expectedResult);
     }
 
     @Test
     public static void EnsureDifferentObjectsEventEmployeeAreNotEqual() {
-        EventEmployee eEmployee = new EventEmployee("Miguel", "miguel@gmail.com", "miguel4", "miguel123");
+        EventEmployee eEmployee = new EventEmployee("Miguel", "miguel@gmail.com", "miguel4", "miguel123",3);
         Object expectedResult = new Object();
         assertNotEquals(eEmployee, expectedResult);
     }
