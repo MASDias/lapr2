c6c117bf7a083b1bd16538b8fd3eda9afb87ec78
MarioDias <MarioDias@LAPTOP-UM1MEFTM>
2017-06-05 11:59:40 +0100

    Stand registry tests [Tests]

--- src/test/java/lapr/project/model/StandRegistryTest.java
+++ src/test/java/lapr/project/model/StandRegistryTest.java
@@ -9,6 +9,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import org.junit.Test;
 
 /**
@@ -27,4 +28,62 @@ public class StandRegistryTest {
         int expectedResult = 2;
         assertEquals(size,expectedResult);
     }
+    
+    @Test
+    public void EnsureSameObjectsStandRegistryIsEqual() throws Exception {
+        StandRegistry result = new  StandRegistry();
+        assertEquals(result, result);
+    }
+
+//    @Test
+//    public void EnsureSameObjectsApplicationAreNotEqual() throws Exception {
+//        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy", Locale.ENGLISH);
+//        Date beginning = sdf.parse("01/10/2017");
+//        Date end = sdf.parse("01/30/2017");
+//        Local local = new Local("Example Street", 500);
+//        Event event = new Event("Model Example", "Explae string", beginning, end, local, null);
+//        Application2 application = new Application2("qwert", event, false);
+//        Application2 application2 = new Application2("qwerty", event, false);
+//
+//        assertNotEquals(application, application2);
+//    }
+//
+//    @Test
+//    public void EnsureDifferentObjectsApplicationAreNotEqual() throws Exception {
+//        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy", Locale.ENGLISH);
+//        Date beginning = sdf.parse("01/10/2017");
+//        Date end = sdf.parse("01/30/2017");
+//        Local local = new Local("Example Street", 500);
+//        Event event = new Event("Model Example", "Explae string", beginning, end, local, null);
+//        Application2 application = new Application2("qwerty", event, false);
+//        Object expectedResult = new Object();
+//        assertNotEquals(application, expectedResult);
+//    }
+//
+//    @Test
+//    public void EnsureToStringIsEqual() throws Exception {
+//        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy", Locale.ENGLISH);
+//        Date beginning = sdf.parse("01/10/2017");
+//        Date end = sdf.parse("01/30/2017");
+//        Local local = new Local("Example Street", 500);
+//        Event event = new Event("Model Example", "Explae string", beginning, end, local, null);
+//        Application2 application = new Application2("qwerty", event, false);
+//        String result = application.toString();
+//        String expectedResult = "Description: qwerty\nTitle: Model Example\nDescription: Explae string\nDate Begin: Tue Jan 10\nDate End: Mon Jan 30\nLocal: Address: Example Street\nDecision: false";
+//        assertEquals(result, expectedResult);
+//    }
+//
+//    @Test
+//    public void EnsureToStringIsNotEqual() throws Exception {
+//        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy", Locale.ENGLISH);
+//        Date beginning = sdf.parse("01/10/2017");
+//        Date end = sdf.parse("01/30/2017");
+//        Local local = new Local("Example Street", 500);
+//        Event event = new Event("Model Example", "Explae string", beginning, end, local, null);
+//        Application2 application = new Application2("qwerty", event, false);
+//        String result = application.toString();
+//        String expectedResult = "Description: qwert\nTitle: Model Example\nDescription: Explae string\nDate Begin: Tue Jan 10\nDate End: Mon Jan 30\nLocal: Address: Example Street\nDecision: false";
+//        assertNotEquals(result, expectedResult);
+//
+//    }
 }
--- src/test/java/lapr/project/model/UserRegistryTest.java
+++ src/test/java/lapr/project/model/UserRegistryTest.java
@@ -26,4 +26,6 @@ public class UserRegistryTest {
         assertEquals(size,expectedResult);
     }
     
+    
+    
 }
