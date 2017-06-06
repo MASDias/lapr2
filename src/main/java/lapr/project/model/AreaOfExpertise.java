3e3575b6c2d23516b5c58b4fe0bbfcd8cf35b5d2
Mario Dias [1151708] <1151708@isep.ipp.pt>
2017-06-06 11:12:52 +0100

    Area of expertise class[Implementation]

---
+++ src/main/java/lapr/project/model/AreaOfExpertise.java
@@ -0,0 +1,15 @@
+package lapr.project.model;
+
+/**
+ *
+ * @author MarioDias
+ */
+public class AreaOfExpertise {
+
+    private String areaExpertise;
+
+    public AreaOfExpertise(String areaExpertise) {
+        this.areaExpertise = areaExpertise;
+    }
+
+}
---
+++ src/main/java/lapr/project/model/Workshop.java
@@ -0,0 +1,10 @@
+
+package lapr.project.model;
+
+/**
+ *
+ * @author 1161386_1161391_1151708_1151172_1150807_Grupo41
+ */
+public class Workshop {
+    
+}
