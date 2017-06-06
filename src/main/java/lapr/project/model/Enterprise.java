479ccd02d72a9c6d0f1e295c3318cf87c7acb1f0
Paulo Cuino <1151172@isep.ipp.pt>
2017-06-06 11:49:47 +0100

    Class Enterprise [fixes issue #28]

---
+++ src/main/java/lapr/project/model/Enterprise.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lapr.project.model;
+
+/**
+ *
+ * @author 1161386_1161391_1151708_1151172_1150807_Grupo41
+ */
+public class Enterprise {
+    private String name;
+    private String email;
+    private String adress;
+    private int nif;
+    private int contact;
+
+    public Enterprise(String name, String email, String adress, int nif, int contact) {
+        this.name = name;
+        this.email = email;
+        this.adress = adress;
+        this.nif = nif;
+        this.contact = contact;
+    }
+    
+    
+}
