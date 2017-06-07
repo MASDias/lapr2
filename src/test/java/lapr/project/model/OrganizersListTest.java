d5208894522c1d5264504f1a62110a35abbb0292
Tiago Cunha [1150807] <1150807@isep.ipp.pt>
2017-06-07 09:51:51 +0100

    OrganizerList [Test] [fixes issue #41]

--- src/test/java/lapr/project/model/OrganizersListTest.java
+++ src/test/java/lapr/project/model/OrganizersListTest.java
@@ -6,6 +6,7 @@
 package lapr.project.model;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import org.junit.Test;
 
 /**
@@ -13,9 +14,10 @@ import org.junit.Test;
  * @author 1161386_1161391_1151708_1151172_1150807_Grupo41
  */
 public class OrganizersListTest {
-    
+
     @Test
-    public void EnsureAddApplicationMethodAddsApplicationToList() throws Exception{
+    public void EnsureAddApplicationMethodAddsApplicationToList() throws Exception {
+        
         OrganizersList list = new OrganizersList();
         Organizer o1 = new Organizer("Miguel", "miguel@gmail.com,", "miguelSantos", "123456789");
         Organizer o2 = new Organizer("Ricardo", "ricardoReis@gmail.com", "ricardoReis", "987654321");
@@ -23,6 +25,26 @@ public class OrganizersListTest {
         list.addOrganizer(o2);
         int size = list.size();
         int expectedResult = 2;
-        assertEquals(size,expectedResult);
+        assertEquals(size, expectedResult);
     }
+
+    @Test
+    public void EnsureSameObjectsOrganizersListAreEqual() throws Exception {
+        
+        OrganizersList list = new OrganizersList();
+        Organizer organizer = new Organizer("Miguel", "miguel@gmail.com,", "miguelSantos", "123456789");
+        list.addOrganizer(organizer);
+        assertEquals(list, list);
+    }
+
+    @Test
+    public void EnsureSameObjectsApplicationsListAreNotEqual() throws Exception {
+
+        Organizer organizer = new Organizer("Miguel", "miguel@gmail.com,", "miguelSantos", "123456789");
+        OrganizersList list = new OrganizersList();
+        OrganizersList list2 = new OrganizersList();
+        list.addOrganizer(organizer);
+        assertNotEquals(list, list2);
+    }
+
 }
