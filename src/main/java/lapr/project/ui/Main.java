9dbdf76de518ef75197c62461841fa821279e4e7
Mario Dias [1151708] <1151708@isep.ipp.pt>
2017-06-07 17:39:00 +0100

    Update event tests[Tests]

--- src/main/java/lapr/project/ui/Main.java
+++ src/main/java/lapr/project/ui/Main.java
@@ -1,5 +1,9 @@
 package lapr.project.ui;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 import lapr.project.model.CalculatorExample;
 
 /**
@@ -17,9 +21,14 @@ class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         CalculatorExample calculatorExample = new CalculatorExample();
         System.out.println(calculatorExample.sum(3, 5));   
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        Date beginning = sdf.parse("23-08-2017");
+        Date end = sdf.parse("01-10-2017");
+        
+        System.out.println(sdf.format(beginning));
     }
 
 }
