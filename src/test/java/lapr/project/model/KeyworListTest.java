67a85d9db8e8c98aac7b58e27531ba21273e79b0
Tiago Cunha [1150807] <1150807@isep.ipp.pt>
2017-06-13 11:23:56 +0100

    KeywordList [Test] [fixes issue #56]

---
+++ src/test/java/lapr/project/model/KeyworListTest.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lapr.project.model;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import org.junit.Test;
+
+/**
+ *
+ * @author 1161386_1161391_1151708_1151172_1150807_Grupo41
+ */
+public class KeyworListTest {
+
+    @Test
+    public void EnsureAddKeywordMethodAddsLocalToList() throws Exception {
+
+        KeywordList list = new KeywordList();
+        Keyword k1 = new Keyword("123456789");
+        Keyword k2 = new Keyword("qwerty");
+
+        list.addKeyword(k1);
+        list.addKeyword(k2);
+        int size = list.size();
+        int expectedResult = 2;
+        assertEquals(size, expectedResult);
+    }
+
+    /**
+     *
+     * @throws Exception
+     */
+    @Test
+    public void EnsureAddKeywordMethodAddsUserToListNotValid() throws Exception {
+        KeywordList result = new KeywordList();
+        Keyword keyword = new Keyword("123456789");
+        result.addKeyword(keyword);
+        result.addKeyword(keyword);
+        int size = result.size();
+        int expectedResult = 1;
+        assertEquals(size, expectedResult);
+    }
+
+    /**
+     *
+     * @throws Exception
+     */
+    @Test
+    public void EnsureAddKeywordMethodAddsUserToListIndex() throws Exception {
+        KeywordList list = new KeywordList();
+        Keyword result = new Keyword("123456789");
+        list.addKeyword(result);
+        Keyword expectedResult = list.getKeyword(0);
+        assertEquals(result, expectedResult);
+    }
+
+    /**
+     *
+     * @throws Exception
+     */
+    @Test
+    public void EnsureSameObjectsKeywordListAreEqual() throws Exception {
+
+        KeywordList list = new KeywordList();
+        Keyword location = new Keyword("123456789");
+
+        list.addKeyword(location);
+        assertEquals(list, list);
+    }
+
+    /**
+     *
+     * @throws Exception
+     */
+    @Test
+    public void EnsureSameObjectsKeywordListAreNotEqual() throws Exception {
+        Keyword location = new Keyword ("123456789");
+        KeywordList k1 = new KeywordList();
+        KeywordList k2 = new KeywordList();
+        k1.addKeyword(location);
+        assertNotEquals(k1, k2);
+    }
+
+}
