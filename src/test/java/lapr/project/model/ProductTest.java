d70a141f4e8d5545da8a3d788c258ded31fb0cc6
Tiago Cunha [1150807] <1150807@isep.ipp.pt>
2017-06-13 12:07:56 +0100

    Product [Test] [fixes issue #58]

---
+++ src/test/java/lapr/project/model/ProductTest.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lapr.project.model;
+
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import org.junit.Test;
+
+/**
+ *
+ * @author 1161386_1161391_1151708_1151172_1150807_Grupo41
+ */
+public class ProductTest {
+    /**
+     *
+     * @throws Exception
+     */
+    @Test
+    public void EnsureSameObjectsProductIsEqual() throws Exception {
+        Product product = new Product("box");
+        assertEquals(product, product);
+    }
+
+    /**
+     *
+     * @throws Exception
+     */
+    @Test
+    public void EnsureSameObjectsProcuctAreNotEqual() throws Exception {
+        Product product1 = new Product("stick");
+        Product product2 = new Product("box");
+        assertNotEquals(product1, product2);
+    }
+}
