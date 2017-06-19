6e6b528e3a423f7fe1c0069d716017d3cd5914d5
Paulo Cuino [1151172] <1151172@isep.ipp.pt>
2017-06-19 10:00:00 +0100

    enhancement of tests eventemployee [fxes issue #75]

--- src/test/java/lapr/project/model/EventEmployeeListTest.java
+++ src/test/java/lapr/project/model/EventEmployeeListTest.java
@@ -5,6 +5,9 @@
  */
 package lapr.project.model;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
@@ -30,5 +33,19 @@ public class EventEmployeeListTest {
         int size = list.size();
         int expectedResult = 2;
         assertEquals(size,expectedResult);
+    }
+     @Test
+    public void EnsureSetApplicationsListIsEqual() throws Exception {
+        ApplicationList result = new ApplicationList();
+
+        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy", Locale.ENGLISH);
+        Date beginning = sdf.parse("01/10/2017");
+        Date end = sdf.parse("01/30/2017");
+        Location local = new Location("Example Street");
+        Event event = new Event("Model Example", "Explae string", beginning, end, null, null, local, 100);
+
+        event.setApplicationsList(result);
+        ApplicationList expectedResult = event.getApplicationsList();
+        assertEquals(expectedResult, result);
     }
 }
