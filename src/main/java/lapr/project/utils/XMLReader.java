056308a564cf10c59ada11a3aad2b6ee550fdbe5
Rúben Santos [1161391] <1161391@isep.ipp.pt>
2017-06-20 12:01:13 +0100

    XML Perfection and Code adaptation[Implementation]

--- src/main/java/lapr/project/utils/XMLReader.java
+++ src/main/java/lapr/project/utils/XMLReader.java
@@ -55,55 +55,24 @@ public class XMLReader {
     /**
      *
      */
-    private String filepathUsers = "";
+    private String filepathXML = "";
+
 
     /**
      *
      */
-    private String filepathStands = "";
-
+    private final String DEFAULT_FILENAME_XML = "xml/exposicao1_v0.1.xml";
+    
     /**
      *
+     * @param fileXML
      */
-    private String filepathLocations = "";
+    public XMLReader(String fileXML) {
 
-    /**
-     *
-     */
-    private final String DEFAULT_FILENAME_USERS = "xml/UserList.xml";
-
-    /**
-     *
-     */
-    private final String DEFAULT_FILENAME_STANDS = "xml/StandsList.xml";
-
-    /**
-     *
-     */
-    private final String DEFAULT_FILENAME_LOCATIONS = "xml/LocationXml.xml";
-
-    /**
-     *
-     * @param fileUsers
-     * @param fileStands
-     * @param fileLocations
-     */
-    public XMLReader(String fileUsers, String fileStands, String fileLocations) {
-
-        if (!testFilepath(fileUsers)) {
-            filepathUsers = DEFAULT_FILENAME_USERS;
+        if (!testFilepath(fileXML)) {
+            filepathXML = DEFAULT_FILENAME_XML;
         } else {
-            filepathUsers = fileUsers;
-        }
-        if (!testFilepath(fileStands)) {
-            filepathStands = DEFAULT_FILENAME_USERS;
-        } else {
-            filepathStands = fileStands;
-        }
-        if (!testFilepath(fileLocations)) {
-            filepathLocations = DEFAULT_FILENAME_LOCATIONS;
-        } else {
-            filepathLocations = fileLocations;
+            filepathXML = fileXML;
         }
     }
 
@@ -111,107 +80,12 @@ public class XMLReader {
      *
      */
     public XMLReader() {
-        filepathUsers = DEFAULT_FILENAME_USERS;
-        filepathStands = DEFAULT_FILENAME_STANDS;
-        filepathLocations = DEFAULT_FILENAME_LOCATIONS;
+        filepathXML = DEFAULT_FILENAME_XML;
     }
-
-    /**
-     *
-     * @param eventCenter
-     * @return
-     * @throws FileNotFoundException
-     * @throws ParserConfigurationException
-     * @throws SAXException
-     * @throws IOException
-     */
-    /*
-     public EventCenter readValuesFromXML(EventCenter eventCenter) throws FileNotFoundException, ParserConfigurationException, SAXException, IOException{
-     try{
-            
-     File xmlFileUsers = new File(filepathUsers);
-     File xmlFileStands = new File(filepathStands);
-     File xmlFileLocations = new File(filepathLocations);
-            
-            
-     DocumentBuilderFactory documentBuilderFactory
-     = DocumentBuilderFactory.newInstance();
-     DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-     InputSource inputSource = new InputSource();
-     inputSource.setCharacterStream(new FileReader(xmlFileUsers));
-     Document document = documentBuilder.parse(inputSource);
-            
-     NodeList users = document.getElementsByTagName("User");
-     UserRegistry userList = eventCenter.getUserRegistry();
-     for (int i = 0; i < users.getLength(); i++) {
-                
-     Element user = (Element)users.item(i);
-                
-     String nomeStr = user.getElementsByTagName("Name").item(0).getTextContent();
-                
-     String emailStr = user.getElementsByTagName("Email").item(0).getTextContent();
-                
-     String usernameStr = user.getElementsByTagName("Username").item(0).getTextContent();
-                
-     String passwordStr = user.getElementsByTagName("Password").item(0).getTextContent();
-                
-     int key = Integer.parseInt(user.getElementsByTagName("Serialkey").item(0).getTextContent());
-                
-     User u = new User(nomeStr,emailStr,usernameStr,passwordStr);  
-     u.setKey(key);
-     userList.addUser(u);
-     }
-     eventCenter.setUserRegistry(userList);
-            
-     inputSource.setCharacterStream(new FileReader(xmlFileStands));
-     document = documentBuilder.parse(inputSource);
-            
-     NodeList stands = document.getElementsByTagName("Stand");
-     StandRegistry standsList = eventCenter.getStandRegistry();
-     for (int i = 0; i < stands.getLength(); i++) {                
-     Element stand = (Element)stands.item(i);
-                
-     String addressStr = stand.getElementsByTagName("address").item(0).getTextContent();
-                
-     float area = Float.parseFloat(stand.getElementsByTagName("area").item(0).getTextContent());
-                
-     Stand s = new Stand(addressStr, area);
-     standsList.addStand(s);
-     }
-     eventCenter.setStandRegistry(standsList);
-            
-     inputSource.setCharacterStream(new FileReader(xmlFileLocations));
-     document = documentBuilder.parse(inputSource);
-            
-     NodeList locations = document.getElementsByTagName("Location");
-     LocationList locationsList = eventCenter.getLocationList();
-     for (int i = 0; i < locations.getLength(); i++) {
-                
-     Element location = (Element)locations.item(i);
-                
-     String addressStr = location.getElementsByTagName("address").item(0).getTextContent();
-     Location l = new Location(addressStr);
-     locationsList.addLocal(l);
-     }
-     eventCenter.setLocationList(locationsList);
-            
-            
-     } catch (FileNotFoundException e) {
-     System.out.println("Ficheiro não encontrado");
-            
-     } catch (IOException e){
-            
-     } catch (ParserConfigurationException e){
-            
-     } catch (SAXException e){
-            
-     }
-     return eventCenter;
-     }*/
     public EventCenter readValuesFromXML(EventCenter eventCenter) throws FileNotFoundException, ParserConfigurationException, SAXException, IOException, ParseException {
         try {
 
-            File xmlFile = new File("xml/exposicao1_v0.1.xml");
+            File xmlFile = new File(filepathXML);
             Event exposicao1 = new Event("", "", null, null, null, null, null, 0);
 
             DocumentBuilderFactory documentBuilderFactory
