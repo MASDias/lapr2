35dc6c63b04077bc2bd40ecb6d981ee3e962f304
Rúben Santos [1161391] <1161391@isep.ipp.pt>
2017-06-20 16:46:32 +0100

    XMLReader Tests [Test]

--- src/test/java/lapr/project/utils/XMLReaderTest.java
+++ src/test/java/lapr/project/utils/XMLReaderTest.java
@@ -44,4 +44,55 @@ public class XMLReaderTest {
         assertNotEquals(ec,ec2);
         
     }
+    
+    @Test
+    public void EnsureXMLReaderWithParametersIsEqual() throws Exception{
+        XMLReader xmlFile = new XMLReader("xml/exposicao1_v0.1.xml");
+        EventCenter ec = new EventCenter();
+        ec = xmlFile.readValuesFromXML(ec);
+        EventCenter ec2 = ec;
+        assertEquals(ec,ec2);
+    }
+
+    @Test
+    public void EnsureXMLReaderWithParametersIsNotEqual() throws Exception{
+        XMLReader xmlFile = new XMLReader("xml/exposicao1_v0.1.xml");
+        EventCenter ec = new EventCenter();
+        ec = xmlFile.readValuesFromXML(ec);
+        EventCenter ec2 = new EventCenter();
+        ec2 = xmlFile.readValuesFromXML(ec2);
+        assertNotEquals(ec,ec2);
+    }
+    
+    @Test
+    public void EnsureTestFilePathIsEqual() throws Exception{
+        XMLReader xmlFile = new XMLReader("xml/exposicao1_v0.1.xml");
+        boolean result = xmlFile.testFilepath("xml/exposicao1_v0.1.xml");
+        boolean expectedResult = true;
+        assertEquals(result,expectedResult);
+    }
+    
+    @Test
+    public void EnsureTestFilePathIsNotEqual() throws Exception{
+        XMLReader xmlFile = new XMLReader("xml/exposicao1_v0.1.xml");
+        boolean result = xmlFile.testFilepath("xml/exposicao1_v0.1.xml");
+        boolean expectedResult = false;
+        assertNotEquals(result,expectedResult);
+    }
+    
+    @Test
+    public void EnsureFilePathTestingWorksInConstrutorIsEqual() throws Exception{
+        XMLReader xmlFile = new XMLReader("xml/exposicao1_v0.xml");
+        boolean result = xmlFile.testFilepath("xml/exposicao1_v0.1.xml");
+        boolean expectedResult = true;
+        assertEquals(result,expectedResult);
+    }
+    
+        @Test
+    public void EnsureFilePathTestingWorksInConstrutorIsNotEqual() throws Exception{
+        XMLReader xmlFile = new XMLReader("xml/exposicao1_v0.xml");
+        boolean result = xmlFile.testFilepath("xml/exposicao1_v0.1.xml");
+        boolean expectedResult = false;
+        assertNotEquals(result,expectedResult);
+    }
 }
