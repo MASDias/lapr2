f8f24f0e43528dc6fdfd90377801ac7b9cff288a
Rúben Santos [1161391] <1161391@isep.ipp.pt>
2017-06-21 13:08:05 +0100

    showEventAcceptanceRate [Implementation]

--- src/main/java/lapr/project/model/ShowEventAcceptanceRate.java
+++ src/main/java/lapr/project/model/ShowEventAcceptanceRate.java
@@ -10,30 +10,32 @@ package lapr.project.model;
  * @author MarioDias
  */
 public class ShowEventAcceptanceRate {
-private Event event;
+
+    private Event event;
     private int accepted;
     private int total;
     private double acceptanceRate;
 
     public ShowEventAcceptanceRate(Event event) {
         this.event = event;
-        ShowEventAcceptanceRate();
+        showEventAcceptanceRate();
     }
-    
-    private  void ShowEventAcceptanceRate() {
+
+    private void showEventAcceptanceRate() {
         int acceptedCounter = 0;
         int totalCounter = 0;
 
         for (int i = 0; i < event.getApplicationsList().size(); i++) {
-            if(event.getApplicationsList().size()>0){
+
             Application application = event.getApplicationsList().getApplication(i);
             totalCounter++;
             if (application.isDecision()) {
                 acceptedCounter++;
             }
-            }else{
-                totalCounter = 1;
-            }
+
+        }
+        if (totalCounter == 0) {
+            totalCounter = 1;
         }
         total = totalCounter;
         accepted = acceptedCounter;
