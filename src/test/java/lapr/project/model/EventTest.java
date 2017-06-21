050c9e4ec25efcdd6b41e75b4b6230f981b27ad0
Tiago Cunha [1150807] <1150807@isep.ipp.pt>
2017-06-21 09:15:29 +0100

    Event [Test] [fixes issue #88]

--- src/test/java/lapr/project/model/EventTest.java
+++ src/test/java/lapr/project/model/EventTest.java
@@ -5,6 +5,7 @@
  */
 package lapr.project.model;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import static org.junit.Assert.assertEquals;
@@ -26,9 +27,9 @@ public class EventTest {
         SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
         Date beginning = sdf.parse("01-10-2017");
         Date end = sdf.parse("10-10-2017");
-        
+
         Location local = new Location("Example Street");
-        Event event = new Event("Model Example", "Explae string", beginning, end, null, null, local,100);
+        Event event = new Event("Model Example", "Explae string", beginning, end, null, null, local, 100);
         assertEquals(event, event);
     }
 
@@ -42,9 +43,9 @@ public class EventTest {
         Date beginning = sdf.parse("01-10-2017");
         Date end = sdf.parse("10-10-2017");
         Location local = new Location("Example Street");
-        
-        Event event = new Event("Model Example", "Example string", beginning, end, null, null, local,100);
-        Event result = new Event("Model", "string", end, beginning, null, null, local,100);
+
+        Event event = new Event("Model Example", "Example string", beginning, end, null, null, local, 100);
+        Event result = new Event("Model", "string", end, beginning, null, null, local, 100);
         assertNotEquals(event, result);
     }
 
@@ -58,9 +59,325 @@ public class EventTest {
         Date beginning = sdf.parse("01-10-2017");
         Date end = sdf.parse("10-10-2017");
         Location local = new Location("Example Street");
-        
-        Event event = new Event("Model Example", "Example string", beginning, end, null, null, local,100);
+
+        Event event = new Event("Model Example", "Example string", beginning, end, null, null, local, 100);
         Object result = new Object();
         assertNotEquals(event, result);
+    }
+//      @Test
+//    public void EnsureGetKeywordListIsEqual() throws Exception {
+//       SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+//        Date beginning = sdf.parse("01-10-2017");
+//        Date end = sdf.parse("10-10-2017");
+//        Location local = new Location("Example Street");      
+//        Event event = new Event("Model Example", "Example string", beginning, end, null, null, local,100);
+//        KeywordList keywordList = event.getKeywordsList();
+//        KeywordList result = new KeywordList();
+//        assertEquals(result, keywordList);
+//    }
+
+    @Test
+    public void EnsureGetTitleIsEqual() throws Exception {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        Date beginning = sdf.parse("01-10-2017");
+        Date end = sdf.parse("10-10-2017");
+        Location local = new Location("Example Street");
+        Event event = new Event("Model Example", "Example string", beginning, end, null, null, local, 100);
+        String result = event.getTitle();
+        String expectedResult = "Model Example";
+        assertEquals(result, expectedResult);
+    }
+
+    @Test
+    public void EnsureGetTitleIsNotEqual() throws Exception {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        Date beginning = sdf.parse("01-10-2017");
+        Date end = sdf.parse("10-10-2017");
+        Location local = new Location("Example Street");
+        Event event = new Event("Model Example", "Example string", beginning, end, null, null, local, 100);
+        String result = event.getTitle();
+        String expectedResult = "Model";
+        assertNotEquals(result, expectedResult);
+    }
+
+    @Test
+    public void EnsureGetLocalIsEqual() throws Exception {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        Date beginning = sdf.parse("01-10-2017");
+        Date end = sdf.parse("10-10-2017");
+        Location local = new Location("Example Street");
+        Event event = new Event("Model Example", "Example string", beginning, end, null, null, local, 100);
+        Location result = event.getLocal();
+        assertEquals(result, local);
+    }
+
+    @Test
+    public void EnsureGetLocalIsNotEqual() throws Exception {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        Date beginning = sdf.parse("01-10-2017");
+        Date end = sdf.parse("10-10-2017");
+        Location local = new Location("Example Street");
+        Event event = new Event("Model Example", "Example string", beginning, end, null, null, local, 100);
+        Location result = event.getLocal();
+        Location expectedResult = new Location("street");
+        assertNotEquals(result, expectedResult);
+    }
+
+    @Test
+    public void EnsureGetEventBeginIsEqual() throws Exception {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        Date beginning = sdf.parse("01-10-2017");
+        Date end = sdf.parse("10-10-2017");
+        Location local = new Location("Example Street");
+        Event event = new Event("Model Example", "Example string", beginning, end, null, null, local, 100);
+        Date result = event.getEventBegin();
+        assertEquals(result, beginning);
+    }
+
+    @Test
+    public void EnsureGetEventBeginIsNotEqual() throws Exception {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        Date beginning = sdf.parse("01-10-2017");
+        Date end = sdf.parse("10-10-2017");
+        Location local = new Location("Example Street");
+        Event event = new Event("Model Example", "Example string", beginning, end, null, null, local, 100);
+        Date result = event.getEventBegin();
+        assertNotEquals(result, end);
+    }
+
+    @Test
+    public void EnsureGetEventEndIsEqual() throws Exception {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        Date beginning = sdf.parse("01-10-2017");
+        Date end = sdf.parse("10-10-2017");
+        Location local = new Location("Example Street");
+        Event event = new Event("Model Example", "Example string", beginning, end, null, null, local, 100);
+        Date result = event.getEventEnd();
+        assertEquals(result, end);
+    }
+
+    @Test
+    public void EnsureGetEventEndIsNotEqual() throws Exception {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        Date beginning = sdf.parse("01-10-2017");
+        Date end = sdf.parse("10-10-2017");
+        Location local = new Location("Example Street");
+        Event event = new Event("Model Example", "Example string", beginning, end, null, null, local, 100);
+        Date result = event.getEventEnd();
+        assertNotEquals(result, beginning);
+    }
+
+    @Test
+    public void EnsureGetOrganizerListIsEqual() throws Exception {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        Date beginning = sdf.parse("01-10-2017");
+        Date end = sdf.parse("10-10-2017");
+        Location local = new Location("Example Street");
+        Event event = new Event("Model Example", "Example string", beginning, end, null, null, local, 100);
+        User user = new User("Miguel", "miguel@gmail.com", "miguel4", "miguel123");
+        Organizer organizer = new Organizer(user);
+        OrganizersList list = new OrganizersList();
+        list.addOrganizer(organizer);
+        event.setOrganizerList(list);
+        OrganizersList result = event.getOrganizerList();
+        assertEquals(result, list);
+    }
+
+    @Test
+    public void EnsureGetOrganizerListIsNotEqual() throws Exception {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        Date beginning = sdf.parse("01-10-2017");
+        Date end = sdf.parse("10-10-2017");
+        Location local = new Location("Example Street");
+        Event event = new Event("Model Example", "Example string", beginning, end, null, null, local, 100);
+        User user = new User("Miguel", "miguel@gmail.com", "miguel4", "miguel123");
+        Organizer organizer = new Organizer(user);
+        OrganizersList list = new OrganizersList();
+        OrganizersList list2 = new OrganizersList();
+        list.addOrganizer(organizer);
+        event.setOrganizerList(list);
+        OrganizersList result = event.getOrganizerList();
+        assertNotEquals(result, list2);
+    }
+
+    @Test
+    public void EnsureSetOrganizerListIsEqual() throws Exception {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        Date beginning = sdf.parse("01-10-2017");
+        Date end = sdf.parse("10-10-2017");
+        Location local = new Location("Example Street");
+        Event event = new Event("Model Example", "Example string", beginning, end, null, null, local, 100);
+        User user = new User("Miguel", "miguel@gmail.com", "miguel4", "miguel123");
+        Organizer organizer = new Organizer(user);
+        OrganizersList list = new OrganizersList();
+        list.addOrganizer(organizer);
+        event.setOrganizerList(list);
+        OrganizersList result = event.getOrganizerList();
+        assertEquals(result, list);
+    }
+
+    @Test
+    public void EnsureSetOrganizerListIsNotEqual() throws Exception {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        Date beginning = sdf.parse("01-10-2017");
+        Date end = sdf.parse("10-10-2017");
+        Location local = new Location("Example Street");
+        Event event = new Event("Model Example", "Example string", beginning, end, null, null, local, 100);
+        User user = new User("Miguel", "miguel@gmail.com", "miguel4", "miguel123");
+        Organizer organizer = new Organizer(user);
+        OrganizersList list = new OrganizersList();
+        OrganizersList list2 = new OrganizersList();
+        list.addOrganizer(organizer);
+        event.setOrganizerList(list);
+        OrganizersList result = event.getOrganizerList();
+        assertNotEquals(result, list2);
+    }
+
+    @Test
+    public void EnsureGetEventEmployeeListIsEqual() throws Exception {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        Date beginning = sdf.parse("01-10-2017");
+        Date end = sdf.parse("10-10-2017");
+        Location local = new Location("Example Street");
+        Event event = new Event("Model Example", "Example string", beginning, end, null, null, local, 100);
+        User user = new User("Miguel", "miguel@gmail.com", "miguel4", "miguel123");
+        EventEmployee eventEmployee = new EventEmployee(user, 3);
+        EventEmployeeList list = new EventEmployeeList();
+        list.addEmployee(eventEmployee);
+        event.setEventEmployeeList(list);
+        EventEmployeeList result = event.getEventEmployeeList();
+        assertEquals(result, list);
+    }
+
+    @Test
+    public void EnsureGetEventEmployeeListIsNotEqual() throws Exception {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        Date beginning = sdf.parse("01-10-2017");
+        Date end = sdf.parse("10-10-2017");
+        Location local = new Location("Example Street");
+        Event event = new Event("Model Example", "Example string", beginning, end, null, null, local, 100);
+        User user = new User("Miguel", "miguel@gmail.com", "miguel4", "miguel123");
+        EventEmployee eventEmployee = new EventEmployee(user, 3);
+        EventEmployeeList list = new EventEmployeeList();
+        EventEmployeeList list2 = new EventEmployeeList();
+        list.addEmployee(eventEmployee);
+        event.setEventEmployeeList(list);
+        EventEmployeeList result = event.getEventEmployeeList();
+        assertNotEquals(result, list2);
+    }
+
+    @Test
+    public void EnsureSetEventEmployeeListIsEqual() throws Exception {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        Date beginning = sdf.parse("01-10-2017");
+        Date end = sdf.parse("10-10-2017");
+        Location local = new Location("Example Street");
+        Event event = new Event("Model Example", "Example string", beginning, end, null, null, local, 100);
+        User user = new User("Miguel", "miguel@gmail.com", "miguel4", "miguel123");
+        EventEmployee eventEmployee = new EventEmployee(user, 3);
+        EventEmployeeList list = new EventEmployeeList();
+        list.addEmployee(eventEmployee);
+        event.setEventEmployeeList(list);
+        EventEmployeeList result = event.getEventEmployeeList();
+        assertEquals(result, list);
+    }
+
+    @Test
+    public void EnsureSetEventEmployeeListIsNotEqual() throws Exception {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        Date beginning = sdf.parse("01-10-2017");
+        Date end = sdf.parse("10-10-2017");
+        Location local = new Location("Example Street");
+        Event event = new Event("Model Example", "Example string", beginning, end, null, null, local, 100);
+        User user = new User("Miguel", "miguel@gmail.com", "miguel4", "miguel123");
+        EventEmployee eventEmployee = new EventEmployee(user, 3);
+        EventEmployeeList list = new EventEmployeeList();
+        EventEmployeeList list2 = new EventEmployeeList();
+        list.addEmployee(eventEmployee);
+        event.setEventEmployeeList(list);
+        EventEmployeeList result = event.getEventEmployeeList();
+        assertNotEquals(result, list2);
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void EnsureToStringIsEqual() throws Exception {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        Date beginning = sdf.parse("01-10-2017");
+        Date end = sdf.parse("10-10-2017");
+        Location local = new Location("Example Street");
+        Event event = new Event("Model Example", "Example string", beginning, end, null, null, local, 100);
+        String result = event.toString();
+        String expectedResult = "Title: Model Example";
+        assertEquals(result, expectedResult);
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void EnsureToStringIsNotEqual() throws Exception {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        Date beginning = sdf.parse("01-10-2017");
+        Date end = sdf.parse("10-10-2017");
+        Location local = new Location("Example Street");
+        Event event = new Event("Model Example", "Example string", beginning, end, null, null, local, 100);
+        String result = event.toString();
+        String expectedResult = "Title: Model";
+        assertNotEquals(result, expectedResult);
+    }
+
+    @Test
+    public void EnsureValidateDateIsValid() throws Exception {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        Date beginning = sdf.parse("01-10-2017");
+        Date end = sdf.parse("10-10-2017");
+        Location local = new Location("Example Street");
+        Event event = new Event("Model Example", "Example string", beginning, end, null, null, local, 100);
+        boolean result = event.validateDate();
+        boolean expectedResult = true;
+        assertEquals(result, expectedResult);
+    }
+
+    @Test
+    public void EnsureValidateDateIsNotValid() throws Exception {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        Date beginning = sdf.parse("10-10-2017");
+        Date end = sdf.parse("01-10-2017");
+        Location local = new Location("Example Street");
+        Event event = new Event("Model Example", "Example string", beginning, end, null, null, local, 100);
+        boolean result = event.validateDate();
+        boolean expectedResult = false;
+        assertEquals(result, expectedResult);
+    }
+
+    @Test
+    public void EnsureValidateSubmissionDateIsValid() throws Exception {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        Date beginning = sdf.parse("01-10-2017");
+        Date end = sdf.parse("10-10-2017");
+        Date submissionBegin = sdf.parse("31-09-2017");
+        Date submissionEnd = sdf.parse("09-10-2017");
+        Location local = new Location("Example Street");
+        Event event = new Event("Model Example", "Example string", beginning, end, submissionBegin, submissionEnd, local, 100);
+        boolean result = event.validateSubmissionDate();
+        boolean expectedResult = true;
+        assertEquals(result, expectedResult);
+    }
+
+    @Test
+    public void EnsureValidateSubmissionDateIsNotValid() throws Exception {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        Date beginning = sdf.parse("01-10-2017");
+        Date end = sdf.parse("10-10-2017");
+        Date submissionBegin = sdf.parse("31-09-2017");
+        Date submissionEnd = sdf.parse("11-10-2017");
+        Location local = new Location("Example Street");
+        Event event = new Event("Model Example", "Example string", beginning, end, submissionBegin, submissionEnd, local, 100);
+        boolean result = event.validateSubmissionDate();
+        boolean expectedResult = false;
+        assertEquals(result, expectedResult);
     }
 }
